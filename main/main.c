6305a04bde8cf04dea36ac9a49a40e47e2254e29
Dominic Dietz <d0mm366@protonmail.com>
2026-01-21 16:29:37 +0100

    Hello Git

--- main/main.c
+++ main/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ds18b20.h"
+
+#define ds18b20_PIN 4;
+
+
+static const char *TAG = "SensorNode";
 
 void app_main(void)
 {
 
+    while (1)
+    {
+        ESP_LOGI(TAG, "Hello Git");
+        vTaskDelay(1000);
+    }
 }
